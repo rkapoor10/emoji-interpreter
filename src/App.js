45d60637aa10365772171f61a363c373e6c88057
RITIK KAPOOR <61417822+rkapoor10@users.noreply.github.com>
2021-06-27 19:30:23 +0530

    edit: added dictionary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,80 @@
+import React, { useState } from "react";
 import "./styles.css";
+var color = "blue";
+var likeCounter = 0;
+
+var emojiDictionary = {
+  "😁": "Beaming Face with Smiling Eyes",
+  "😅": "Grinning Face with Sweat",
+  "🤣": "Rolling on the Floor Laughing",
+  "😂": "Face with Tears of Joy",
+  "🙂": "Slightly Smiling Face",
+  "🙃": "Upside-Down Face",
+  "😉": "Winking Face",
+  "😊": "Smiling Face with Smiling Eyes",
+  "😇": "Smiling Face with Halo",
+  "😍": "Smiling Face with Heart-Eyes",
+  "🤩": "Star-Struck",
+  "😘": "Face Blowing a Kiss",
+  "😜": "Winking Face with Tongue",
+  "🤪": "Zany Face",
+  "😝": "Squinting Face with Tongue",
+  "🤑": "Money-Mouth Face",
+  "🤗": "Hugging Face",
+  "🤐": "Zipper-Mouth Face",
+  "😑": "Expressionless Face",
+  "😏": "Smirking Face",
+  "😒": "Unamused Face",
+  "🙄": "Face with Rolling Eyes",
+  "😬": "Grimacing Face",
+  "🤥": "Lying Face",
+  "😌": "Relieved Face",
+  "😴": "Sleeping Face",
+  "😷": "Face with Medical Mask",
+  "🤒": "Face with Thermometer",
+  "🤢": "Nauseated Face",
+  "🤮": "Face Vomiting",
+  "🤧": "Sneezing Face",
+  "🥵": "Hot Face",
+  "🥶": "Cold Face",
+  "😵": "Dizzy Face",
+  "🤯": "Exploding Head",
+  "🥳": "Partying Face"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
 
 export default function App() {
+  function likeClickHandler() {
+    likeCounter = likeCounter + 1;
+    console.log("clicked!", likeCounter);
+    setLikeCounter(likeCounter);
+  }
+
+  var [userInput, setUserInput] = useState("");
+
+  function onChangeHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+    if (meaning === undefined) meaning = "emoji not present in database";
+    setUserInput(meaning);
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Edit to see some magic happen!</h2>
+      <h1 style={{ color, backgroundColor: "yellow" }}>EMOJI INTERPRETER</h1>
+      <h2>Paste emoji in the tab below or click the emoji to know meaning </h2>
+      <h2>
+        {emojisWeKnow.map(function (emoji) {
+          return (
+            <span style={{ fontSize: "2rem" }} key={emoji}>
+              {emoji}
+            </span>
+          );
+        })}
+      </h2>
+      <input onChange={onChangeHandler}></input>
+      <div style={{ color }}>{userInput}</div>
     </div>
   );
 }
